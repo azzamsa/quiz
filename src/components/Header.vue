f1a3974b65589a4b03665f94d8d610193ea3b50e
azzamsa <oktinastuzi@pm.me>
2019-12-13 21:50:23 +0700

    add: basic skeleton

---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <b-nav tabs>
+      <b-nav-item disabled>Azra Quiz App</b-nav-item>
+      <b-nav-item disabled>Counter: 1/10</b-nav-item>
+    </b-nav>
+  </div>
+</template>
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="question-box-container">
+   <b-jumbotron>
+    <template v-slot:lead>
+      Questions here
+    </template>
+
+    <hr class="my-4">
+
+    <p>
+      list of anwers
+    </p>
+
+    <b-button variant="primary" href="#">Submit</b-button>
+    <b-button variant="success" href="#">Next</b-button>
+  </b-jumbotron>
+</div>
+</template>
