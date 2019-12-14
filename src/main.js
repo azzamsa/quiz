c86ae3b33b991d183d75fc3449d5a37cbccef944
azzamsa <oktinastuzi@pm.me>
2019-12-14 23:27:05 +0700

    add: vuex

--- src/main.js
+++ src/main.js
@@ -8,10 +8,12 @@ Vue.use(BootstrapVue)
 
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
